3af683ee858d40520367a90d4d14703d52248c46
PinguBerry <pinguberrycoraline@gmail.com>
2026-06-15 13:57:09 -0300

    mãozinhas teclando juntas + confirmação

--- testepassopiano.ino
+++ testepassopiano.ino
@@ -1,63 +1,71 @@
+// ARDUINO MEGA
+
 #include <VarSpeedServo.h>
 #include <AccelStepper.h>
 
 #define STEP_PIN 22
 #define DIR_PIN 24
 
-/*#define STEP_PIN 3
-#define DIR_PIN 4*/
-
 float dif2 = 3.70;
-float dif1 = 7.45;
+float dif1 = 7.15;
 
 VarSpeedServo s8; // cria o objeto servo
 int angulo = 0; // declara origem do angulo como 0°
 VarSpeedServo s9;
 VarSpeedServo s10;
 
+const char* cifra[] = {"E1", "E1", "FA#", "A1", "A1", "A1", "B1", "DO#", "A1", "A1", "E1", "E1", "FA#", 
+"A1", "A1", "A1", "B1", "B1", "DO#", "B1", "B1"};//21 notas
+
+bool cifra2[] = {0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1};
+
+
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
 void d1p(){
-  s8.write(110, 200, true);
-  delay(50);
+  s8.write(90, 200, true);
+  delay(10);
   s8.write(155, 250, true);
 }
 
 void d1b(){
-  s8.write(45, 200, true);
-  delay(50);
+  s8.write(15, 250, true);
+  delay(10);
   s8.write(155, 250, true);
 }
 
 void d2p(){
-  s9.write(110, 200, true);
-  delay(50);
+  s9.write(90, 250, true);
+  delay(10);
   s9.write(155, 250, true);
 }
+
 void d2b(){
-  s9.write(45, 200, true);
-  delay(50);
+  s9.write(15, 250, true);
+  delay(10);
   s9.write(155, 250, true);
 }
 
 void d3p(){
-  s10.write(110, 200, true);
-  delay(50);
+  s10.write(90, 250, true);
+  delay(10);
 
   s10.write(155, 250, true);
   //delay(100);
 }
 
 void d3b(){
-  s10.write(45, 200, true);
-  delay(50);
+  s10.write(15, 250, true);
+  delay(10);
 
   s10.write(155, 250, true);
   //delay(500);
 }
 
+
 void setup() {
-  Serial.begin(9600);
+  delay(5000);
+  Serial1.begin(9600);
   pinMode(26, INPUT_PULLUP);
 
   s8.attach(8); //declara pino digital utilizado
@@ -68,11 +76,11 @@ void setup() {
   s9.write(180, 50, true);
   s10.write(180, 50, true);
 
-  stepper.setMaxSpeed(30000);
+  stepper.setMaxSpeed(20000);//1000000
 
   //----------- HOMING --------------
 
-  stepper.setSpeed(-9500);
+  stepper.setSpeed(5000);
 
   while (digitalRead(26) == HIGH) {//low é apertado
     stepper.runSpeed();
@@ -80,7 +88,7 @@ void setup() {
 
   stepper.setCurrentPosition(0);
 
-  stepper.setAcceleration(15000);//4000
+  stepper.setAcceleration(20000);//2000000 
 
   s8.write(180, 70, true);
   s9.write(180, 70, true);
@@ -89,17 +97,17 @@ void setup() {
 }
 
 float passos(float dist){//com base no motor 3
-  return (3200 * dist)/8.00;
+  return -((6400 * dist)/31.80);
 }
 
 float distancia(const char* tecla){
-  if(tecla == "E1") return 35.70;
-  if(tecla == "FA#") return 33.60;
-  if(tecla == "SOL#") return 31.40;
-  if(tecla == "A1") return 30.30;
-  if(tecla == "B1") return 28.10;
-  if(tecla == "DO#") return 26.00;
-  if(tecla == "D2") return 24.80;
+  if(tecla == "E1") return 35.60;
+  if(tecla == "FA#") return 33.40;
+  if(tecla == "SOL#") return 31.20;
+  if(tecla == "A1") return 30.10;
+  if(tecla == "B1") return 28.00;
+  if(tecla == "DO#") return 25.90;
+  if(tecla == "D2") return 25.00;
 
   return -1;
 }
@@ -108,15 +116,29 @@ void move(int dedo, const char* tecla){
 
   if(dedo == 1){
     stepper.runToNewPosition(passos(distancia(tecla) - dif1));
+
+  } else if(dedo == 2){
+    stepper.runToNewPosition(passos(distancia(tecla) - dif2));
+
+  } else if(dedo == 3){
+    stepper.runToNewPosition(passos(distancia(tecla)));
+  }
+
+}
+
+void toca(int dedo, const char* tecla){
+  if(dedo == 1){
+
     if(tecla == "E1" or tecla == "A1" or tecla == "B1" or tecla == "D2"){
       d1b();
     }
+
     if(tecla == "FA#" or tecla == "SOL#" or tecla == "DO#"){
       d1p();
     }
 
   } else if(dedo == 2){
-    stepper.runToNewPosition(passos(distancia(tecla) - dif2));
+
     if(tecla == "E1" or tecla == "A1" or tecla == "B1" or tecla == "D2"){
       d2b();
     }
@@ -125,7 +147,6 @@ void move(int dedo, const char* tecla){
     }
 
   } else if(dedo == 3){
-    stepper.runToNewPosition(passos(distancia(tecla)));
     
     if(tecla == "E1" or tecla == "A1" or tecla == "B1" or tecla == "D2"){
       d3b();
@@ -134,8 +155,13 @@ void move(int dedo, const char* tecla){
       d3p();
     }
   }
-
 }
+
+int i = 0;
+int men;
+bool chegou = 0;
+bool muda = 0;
+bool ini = 0;
 
 void loop() {
   //s10.write(125,70, true);
@@ -143,25 +169,194 @@ void loop() {
   //s10.write(180, 70, true);
   //delay(100);
   //s10.write(180, 200, true);
-  
-  /*move(3, "E1");
-  move(3, "E1");
-  move(3, "FA#");
-  move(3, "SOL#");
-  move(3, "A1");
-  move(3, "B1");
-  move(3, "DO#");
-  move(3, "D2");*/
-  move(1, "E1");
-  move(1, "E1");
+
+  /*for(int i = 0; i < 4; i++){
+    move(3, cifra[i]);
+
+    if(cifra2[i] == 1){
+      Serial1.println("1");
+    }
+
+    toca(3, cifra[i]);
+
+  }*/
+
+  for(int i = 0; i < 21; i++){
+    move(3, cifra[i]);
+
+    if(cifra2[i] == 1){
+      Serial1.println("3");//pergunta chegou?
+      
+      while(Serial.available() > 0 && Serial.peek() != '2') Serial.read();
+
+      Serial1.println("1");
+    }
+
+    toca(3, cifra[i]);
+
+  }
+
+
+ /* stepper.run();
+
+  if(i < 1){
+
+    if(ini == 0){
+      move(1, cifra[i]);
+      ini = 1;
+    }
+    
+    move(1, cifra[i]);
+
+    if(cifra2[i] == 1 && chegou == 1 && stepper.distanceToGo() == 0){
+      Serial1.println("1");
+      
+      toca(1, cifra[i]);
+      chegou = 0;
+      muda = 1;
+      ini = 0;
+    }
+
+    if(cifra2[i] == 0 && stepper.distanceToGo() == 0){
+      toca(1, cifra[i]);
+      muda = 1;
+      ini = 0;
+    }
+
+    if(i < 21 && muda == 1) {
+      i++; //já fica na posição
+      muda = 0;
+    }
+
+  }
+
+  if(Serial1.available() > 0){
+    men =  Serial1.read();
+
+    if(men == '2'){//é pra tocar
+      chegou = 1;
+    }
+  }
+
+
+  /*move(2, "E1");
+  move(2, "FA#");
+  move(2, "SOL#");
+  move(2, "A1");
+  move(2, "B1");
+  move(2, "DO#");
+  move(2, "D2");*/
+  /*move(2, "E1");
+  move(2, "E1");
   move(2, "FA#");
   move(2, "A1");
   move(2, "A1");
   move(2, "A1");
-  move(3, "B1");
-  move(3, "DO#");
+  move(2, "B1");
+  move(2, "DO#");
   move(2, "A1");
   move(2, "A1");
+  //making up a song about coralinee
+  delay(500);
+
+  move(2, "E1");
+  move(2, "E1");
+  move(2, "FA#");
+  //shes a peach
+
+  delay(500);
+
+  move(2, "A1");
+  move(2, "A1");
+  move(2, "A1");
+
+  //she's a doll
+
+  delay(500);
+
+  move(2, "B1");
+  move(2, "B1");
+  move(2, "DO#");
+
+  //she's a pal
+
+  move(2, "B1");
+  move(2, "B1");
+  
+  //of mine
+
+  delay(500);
+
+  move(2, "D2");
+  move(2, "D2");
+  move(2, "D2");
+
+  //she's as cute
+
+  move(2, "B1");
+  move(2, "B1");
+
+  //as a
+
+  move(2, "A1");
+  move(2, "A1");
+
+  //buttom
+
+  move(2, "SOL#");
+  move(2, "SOL#");
+
+  //in the
+
+  move(2, "A1");
+
+  //eyes
+
+  move(2, "DO#");
+
+  //of
+
+  move(2, "B1");
+  move(2, "B1");
+
+  //everyone
+
+  move(2, "A1");
+  move(2, "A1");
+
+  //who
+
+  move(2, "SOL#");
+  move(2, "SOL#");
+
+  //ever 
+
+
+  move(2, "FA#");
+  move(2, "FA#");
+
+  //laid their
+
+  move(2, "E1");
+
+  //eyes
+
+  move(2, "FA#");
+
+  //on
+
+  move(2, "A1");
+
+  move(2, "SOL#");
+
+  move(2, "A1");
+
+  //coralinee
+
+
+  delay(2000000);
+
+
 
   //move(3, "E1");
   //move(3, "E1");
--- testepiano2.ino
+++ testepiano2.ino
@@ -1,100 +1,112 @@
+//ARDUINO UNO
+
 #include <VarSpeedServo.h>
 #include <AccelStepper.h>
 
-#define STEP_PIN 22
-#define DIR_PIN 24
+#define STEP_PIN 6
+#define DIR_PIN 5
 
 /*#define STEP_PIN 3
 #define DIR_PIN 4*/
 
 //mudar para a maozinha 2
 float dif2 = 3.70;
-float dif1 = 7.45;
+float dif1 = 7.15;
 
-VarSpeedServo s8; // cria o objeto servo
 int angulo = 0; // declara origem do angulo como 0°
-VarSpeedServo s9;
-VarSpeedServo s10;
+
+int men;
+int cont = 0;
+bool chegou = false;
+
+VarSpeedServo s11; // cria o objeto servo
+VarSpeedServo s12;
+VarSpeedServo s13;
 
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
+const char* cifra[] = {"FA#1", "D", "A", "FA#1", "FA#1", "D", "B", "D"};
+
 void d1p(){
-  s8.write(110, 200, true);
-  delay(50);
-  s8.write(155, 250, true);
+  s13.write(100, 200, true);
 }
 
 void d1b(){
-  s8.write(45, 200, true);
-  delay(50);
-  s8.write(155, 250, true);
+  s13.write(40, 200, true);
 }
 
 void d2p(){
-  s9.write(110, 200, true);
-  delay(50);
-  s9.write(155, 250, true);
+  s12.write(90, 250, true);
+  delay(10);
+  s12.write(155, 250, true);
 }
+
 void d2b(){
-  s9.write(45, 200, true);
-  delay(50);
-  s9.write(155, 250, true);
+  s12.write(15, 250, true);
+  delay(10);
+  s12.write(155, 250, true);
 }
 
 void d3p(){
-  s10.write(110, 200, true);
-  delay(50);
-
-  s10.write(155, 250, true);
-  //delay(100);
+  s11.write(90, 250, true);
+  delay(10);
+  s11.write(155, 250, true);
 }
 
 void d3b(){
-  s10.write(45, 200, true);
-  delay(50);
-
-  s10.write(155, 250, true);
-  //delay(500);
+  s11.write(15, 250, true);
+  delay(10);
+  s11.write(155, 250, true);
 }
 
 void setup() {
+  delay(5000);
   Serial.begin(9600);
-  pinMode(28, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
 
-  stepper.setMaxSpeed(30000);
+  s11.attach(11);
+  s12.attach(12);
+  s13.attach(13);
+
+  s11.write(180, 50, true);
+  s12.write(180, 50, true);
+  s13.write(180, 50, true);
+
+  stepper.setMaxSpeed(20000);
 
   //----------- HOMING --------------
 
-  stepper.setSpeed(-6000);
+  stepper.setSpeed(5000);
 
-  while (digitalRead(28) == HIGH) {//low é apertado
+  while (digitalRead(7) == HIGH) {//low é apertado
     stepper.runSpeed();
   }
 
   stepper.setCurrentPosition(0);
 
-  stepper.setAcceleration(15000);//4000
+  stepper.setAcceleration(20000);//4000
 
+  s11.write(180, 70, true);
+  s12.write(180, 70, true);
+  s13.write(180, 70, true);
+
+  move(3, cifra[cont]);
 }
 
 float passos(float dist){//com base no motor 3
-  return (3200 * dist)/8.00;
+  return -((3200 * dist)/16.00);
 }
 
 float distancia(const char* tecla){
   //MAOZINHA 1
-  if(tecla == "E1") return 35.70;
-  if(tecla == "FA#") return 33.60;
-  if(tecla == "SOL#") return 31.40;
-  if(tecla == "A1") return 30.30;
-  if(tecla == "B1") return 28.10;
-  if(tecla == "DO#") return 26.00;
-  if(tecla == "D2") return 24.80;
+  if(tecla == "E1") return 30.70;
+  if(tecla == "E2") return 44.00;
 
   //MAOZINHA 2
+  
+  if(tecla == "FA#1") return 6.60;
   if(tecla == "A") return 9.80;
   //if(tecla == "S#") return 8.80; nao usa
-  if(tecla == "FA#1") return 6.60;
   if(tecla == "B") return 12.00;
   if(tecla == "D") return 15.30;
   if(tecla == "E") return 17.50;
@@ -105,47 +117,200 @@ float distancia(const char* tecla){
 void move(int dedo, const char* tecla){
 
   if(dedo == 1){
-    stepper.runToNewPosition(passos(distancia(tecla) - dif1));
-    if(tecla == "E1" or tecla == "A1" or tecla == "B1" or tecla == "D2"){
-      d1b();
-    }
-    if(tecla == "FA#" or tecla == "SOL#" or tecla == "DO#"){
-      d1p();
-    }
+    stepper.moveTo(passos(distancia(tecla) - dif1));
 
   } else if(dedo == 2){
-    stepper.runToNewPosition(passos(distancia(tecla) - dif2));
-    if(tecla == "E1" or tecla == "A1" or tecla == "B1" or tecla == "D2"){
-      d2b();
-    }
-    if(tecla == "FA#" or tecla == "SOL#" or tecla == "DO#"){
-      d2p();
-    }
+    stepper.moveTo(passos(distancia(tecla) - dif2));
 
   } else if(dedo == 3){
-    stepper.runToNewPosition(passos(distancia(tecla)));
-    
-    if(tecla == "E1" or tecla == "A1" or tecla == "B1" or tecla == "D2"){
-      d3b();
-    }
-    if(tecla == "FA#" or tecla == "SOL#" or tecla == "DO#"){
-      d3p();
-    }
+    stepper.moveTo(passos(distancia(tecla)));
   }
 
 }
 
+void toca(int dedo, const char* tecla){
+
+  if(dedo == 1){
+    //opa
+    d1p();//pode tirar isso depois
+
+  } else if(dedo == 2){
+
+    if(tecla == "A" or tecla == "B" or tecla == "D" or tecla == "E"){
+      d2b();
+    }
+    if(tecla == "FA#1"){
+      d2p();
+    }
+
+  } else if(dedo == 3){
+    
+    if(tecla == "A" or tecla == "B" or tecla == "D" or tecla == "E"){
+      d3b();
+    }
+    if(tecla == "FA#1"){
+      d3p();
+    }
+  }
+}
+
+bool envia = 0;
+
 void loop() {
-  //s10.write(125,70, true);
+  //s10.write(125,70, true);a
   //delay(100);
   //s10.write(180, 70, true);
   //delay(100);
   //s10.write(180, 200, true);
-  
- // stepper.runToNewPosition(passos(17.50));
+  /*stepper.run();
 
-  //move(3, "E1");
-  //move(3, "E1");
+  if(stepper.distanceToGo() == 0 && chegou == 0){//ele chegou
+    Serial.println("2");
+    chegou = true;
+  }*/
+
+  stepper.run();
+
+  if(stepper.distanceToGo() == 0 && chegou == 0 && envia){//ele chegou
+    Serial.println("2");
+    chegou = true;
+  }
+
+  if(Serial.available() > 0){
+    men = Serial.read();
+
+    if(men == '1'){//é pra tocar
+      toca(3, cifra[cont]);
+
+      if(cont < 7) {
+        cont++;
+        move(3, cifra[cont]);//já fica na posição
+        chegou = 0;
+      } else {
+        cont = 0;
+        move(3, cifra[cont]);//já fica na posição
+        chegou = 0;
+      }
+
+    }
+
+    if(men == '3'){//é pra tocar
+      envia = 1;
+    }
+  }
+
+  /*if(Serial.available() > 0){
+    men = Serial.read();
+
+    if(men == '1'){//é pra tocar
+      toca(3, cifra[cont]);
+
+      if(cont < 1) {
+        cont++;
+        //move(3, cifra[cont]);//já fica na posição
+      }
+
+    }
+
+
+  }*/
+  
+  /*
+
+  stepper.run();
+
+  if(stepper.distanceToGo() == 0 && chegou == 0 && envia){//ele chegou
+    Serial.println("2");
+    chegou = true;
+  }
+
+  if(Serial.available() > 0){
+    men = Serial.read();
+
+    if(men == '1'){//é pra tocar
+      toca(3, cifra[cont]);
+
+      if(cont < 7) {
+        cont++;
+        move(3, cifra[cont]);//já fica na posição
+      }
+
+    }
+
+    if(men == '3'){//é pra tocar
+      envia = 1;
+    }
+  }
+
+  */
+
+ /* FAZER FUNÇÃO
+  move(1, "E2");
+  d1b();
+  move(1, "E1");
+  d1p();
+
+  */
+
+  /*
+  move(3, "A");
+  move(3, "A");
+
+  move(3, "D");
+  move(3, "D");
+
+  move(1, "E2");
+  d1b();
+  move(1, "E1");
+  d1p();
+
+  // making up
+  move(3, "A");
+  move(3, "A");
+
+  move(3, "FA#1");
+  move(3, "FA#1");
+
+  // a song about
+  move(3, "D");
+  move(3, "D");
+
+  //coraline
+  move(3, "A");
+
+  move(3, "FA#1");
+  move(3, "FA#1");
+
+  move(3, "D");
+  move(3, "D");
+
+  //she's a peach
+  move(3, "A");
+  move(3, "A");
+
+  move(3, "FA#1");
+  move(3, "FA#1");
+
+  //she's a doll
+  move(3, "D");
+  move(3, "D");
+
+  //she's a pal of mine
+
+  move(3, "B");
+  move(3, "B");
+
+  move(3, "D");
+  move(3, "D");
+
+  move(3, "E");
+  move(3, "E");
+
+
+
+  delay(2000);*/
+
+
   //move(3, "A1");
 
   //d1b();*/
